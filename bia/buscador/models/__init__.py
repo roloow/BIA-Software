e18528587685dbabcfb74a1d32eff7abe3f36e3c
rCasanueva <rolacasanueva@gmail.com>
2017-06-03 19:55:10 -0400

    cambio de nombre a modelos

--- bia/buscador/models/__init__.py
+++ bia/buscador/models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-from ClientModel import ClientModel
-from KolbModel import KolbModel
-from TagModel import TagModel
-from DataModel import DataModel
-from TypeModel import TypeModel
+from client import ClientModel
+from kolb import KolbModel
+from tag import TagModel
+from data import DataModel
+from types import TypeModel
 from relation_entities import KolbTagModel, DataTagModel, DataTypeModel
