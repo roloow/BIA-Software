cd9c226a6365d3a15cca611e3c77147d495f1fda
rCasanueva <rolacasanueva@gmail.com>
2017-06-06 00:37:45 -0400

    Login / Register / Kolb

--- bia/buscador/urls.py
+++ bia/buscador/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'signin$', views.login_method, name='login'),
+    url(r'signup$', views.register, name='register'),
+    url(r'kolbform$', views.kolb_form, name='kolb'),
 ]
--- bia/buscador/views/client.py
+++ bia/buscador/views/client.py
@@ -1,4 +1,71 @@
 # -*- coding: utf-8 -*-
-"""
-This view controls the client configurations
-"""
+from .common import get_base_context
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from buscador.models import ClientModel, KolbModel
+from django.contrib.auth.models import User
+import urllib
+
+# Login
+def login_method(request):
+    context = get_base_context(request)
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            return render(request, 'buscador/home.html', context)
+        return render(request, 'buscador/user_login.html', context)
+    if request.method == "POST":
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user:
+            login(request, user)
+            return render(request, 'buscador/home.html', context)
+        context['error'] = True
+        return render(request, 'buscador/user_login.html', context)
+
+# Register
+def register(request):
+    context = get_base_context(request)
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            return render(request, 'buscador/home.html', context)
+        return render(request, 'buscador/user_login.html', context)
+    if request.method == "POST":
+        if ClientModel.objects.filter(auth_user__email=request.POST['email']):
+            context['error_registro'] = True
+            return render(request, 'buscador/user_login.html', context)
+        user = User.objects.create_user(request.POST['email'],request.POST['email'],request.POST['password'])
+        user.first_name = request.POST['firstname']
+        user.last_name = request.POST['lastname']
+        user.save()
+        client = ClientModel()
+        client.auth_user = user
+        client.save()
+        login(request, user)
+        response = redirect('buscador:kolb')
+        response['Location'] += '?' + urilib.urlencode({'client_id'})
+        return response
+
+# Kolb Form
+@login_required
+def kolb_form(request):
+    context = get_base_context(request)
+    client_id = request.GET.get("client_id", False)
+    context['client_id'] = client_id
+    print request.user.profile.pk, client_id
+    if request.user.profile.pk != int(client_id):
+        # TODO: PAGE 404 - User cannot do someone elses KolbForm
+        return render(request, 'buscador/home.html', context)
+    if request.method == 'GET':
+        return render(request, 'buscador/kolb_form.html', context)
+    if request.method == 'POST':
+        # TODO: Mejorar comportamiento
+        if 'q1' not in request.POST.keys():
+            context['error'] = True
+            return render(request, 'buscador/kolb_form.html', context)
+        kolb_profile = request.POST['q1']
+        # TODO: Analizador de respuestas
+        kp = KolbModel.objects.get(nombre=kolb_profile)
+        client = ClientModel.objects.get(pk=client_id)
+        client.kolb_profile = kp
+        client.save()
+        return render(request, 'buscador/home.html', context)
