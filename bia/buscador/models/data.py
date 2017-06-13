88b129a017fcc6e1b0a88771ae04fe690a0e7b04
rCasanueva <rolacasanueva@gmail.com>
2017-06-13 04:35:58 -0400

    cambios de modelos (requiere migrar)

--- bia/buscador/models/data.py
+++ bia/buscador/models/data.py
@@ -3,13 +3,14 @@
 from django.db import models
 
 def data_file_handler(instance, filename):
-    data_id = instance.pk
+    data_id = len(DataModel.objects.all()) + 1
     ext = filename.split('.')[-1]
     return "data_{data_id}.{ext}".format(data_id=data_id, ext=ext)
 
 class DataModel(models.Model):
     file_path = models.FileField(upload_to=data_file_handler, blank=True, null=True)
-    nombre = models.CharField(max_length=25)
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=300, null=True, blank=True)
     tags = models.ManyToManyField('TagModel', through='DataTagModel',
                                     through_fields=('data', 'tag'),
                                     related_name='tag_files')
--- bia/buscador/models/kolb.py
+++ bia/buscador/models/kolb.py
@@ -3,6 +3,11 @@
 from django.db import models
 
 class KolbModel(models.Model):
+    ASIMILADOR = 1
+    DIVERGENTE = 2
+    ACOMODADOR = 3
+    CONVERGENTE = 4
+
     nombre = models.CharField(max_length=15)
     description = models.CharField(max_length=100, blank=True, null=True)
     tags = models.ManyToManyField('TagModel',
